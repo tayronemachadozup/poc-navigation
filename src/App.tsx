856025ecad7852c19244f7bf4d0024c5ed97d119
tayronearaujo <tayron_machado@hotmail.com>
2021-11-04 14:57:34 -0300

    navigation poc

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from 'react';
+import { BeagleProvider, BeagleRemoteView } from '@zup-it/beagle-react';
+import BeagleService from './beagle/beagle-service';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App: FC = () => {
+    return (
+        <BeagleProvider value={BeagleService}>
+            <BeagleRemoteView route="/payload.json" />
+        </BeagleProvider>
+    );
 }
 
 export default App;
---
+++ src/beagle/beagle-service.ts
@@ -0,0 +1,6 @@
+import { createBeagleUIService } from '@zup-it/beagle-react'
+
+export default createBeagleUIService({
+  baseUrl: 'http://localhost:3000/',
+  components: {}
+})
